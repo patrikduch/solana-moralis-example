f9003996b9e9d0a937ec9ea876a66f832462bc5b
patrikduch <duchpatrik@icloud.com>
2025-04-25 23:09:21 +0200

    feat: timeseries db

--- db/script.sql
+++ db/script.sql
@@ -3,3 +3,11 @@ CREATE TABLE users (
     name TEXT NOT NULL,
     email TEXT NOT NULL UNIQUE
 );
+
+CREATE TABLE crypto_prices (
+  asset_name TEXT NOT NULL,
+  price NUMERIC NOT NULL,
+  ts TIMESTAMPTZ NOT NULL
+);
+
+SELECT create_hypertable('crypto_prices', 'ts');
